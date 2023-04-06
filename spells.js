d6f5baadc3deaeb5ca5fd501c8a468d40562ef2a
Evelyn House <eve@ehouse.io>
2023-04-06 17:38:59 -0400

    Completed equipment and spell scraper

--- spells.js
+++ spells.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Pathfinder Equipment Scraper
+// @name         Pathfinder Spell Scraper
 // @namespace    http://tampermonkey.net/
 // @version      v0.1
 // @author       Evelyn House (ehouse@fastmail.com)
-// @match        https://2e.aonprd.com/Equipment.aspx
+// @match        https://2e.aonprd.com/Spells.aspx
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=aonprd.com
 // @grant        unsafeWindow
 // ==/UserScript==
@@ -12,7 +12,7 @@
     'use strict';
 
     function exportData(JsonExport) {
-        const filename = 'equipment.json';
+        const filename = 'spells.json';
         const jsonStr = JSON.stringify(JsonExport);
 
         let element = document.createElement('a');
@@ -33,15 +33,26 @@
             name: rowData[0].textContent,
             link: rowData[0].firstChild.href,
             source: {name: rowData[2].querySelector('a').textContent, href: rowData[2].querySelector('a').href},
-            rarity: rowData[3].textContent,
-            traits: Array.from(rowData[4].querySelectorAll('a')).map((anchor) => ({
+            tradition: Array.from(rowData[3].querySelectorAll('a')).map((anchor) => ({
                 name: anchor.textContent, href: anchor.href
             })),
-            category: rowData[5].textContent,
-            subcategory: rowData[6].textContent,
-            level: rowData[7].textContent,
-            price: rowData[8].textContent,
-            bulk: rowData[9].textContent
+            rarity: rowData[4].textContent,
+            trait: Array.from(rowData[5].querySelectorAll('a')).map((anchor) => ({
+                name: anchor.textContent, href: anchor.href
+            })),
+            type: rowData[6].textContent,
+            school: rowData[7].textContent,
+            level: rowData[8].textContent,
+            heighten: rowData[9].textContent,
+            summary: rowData[10].textContent,
+            action: rowData[11].textContent.trim(),
+            component: rowData[12].textContent,
+            trigger: rowData[13].textContent,
+            target: rowData[14].textContent,
+            range: rowData[15].textContent,
+            area: rowData[16].textContent,
+            duration: rowData[17].textContent,
+            saving_throw: rowData[18].textContent,
         }
 
         if (settings.debug) {
